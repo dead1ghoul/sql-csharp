23dcc75b51961f335683b42217026db46aad51ee
dead1ghoul <115005232+dead1ghoul@users.noreply.github.com>
2023-07-13 16:08:43 +0300

    ConnectionStr property has not been initialized

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public string ConnectionString { get; set; }
+        public NpgsqlConnectionStringBuilder builder;
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            builder = new NpgsqlConnectionStringBuilder();
+            builder.Host = DatabaseAddressTextBox.Text;
+            builder.Port = 5432;
+            builder.Username = UsernameTextBox.Text;
+            builder.Password = PasswordBox.Password;
+            builder.Database = "postgres";
+            string connString = builder.ConnectionString;
+            builder.MaxPoolSize = 10;//count of flows
+
+            //using (NpgsqlConnection connection = new NpgsqlConnection(connString))
+            //{
+            //    connection.Open();
+            //    // выполнение операций с подключением
+            //    connection.Close();
+            //}
+
+            ConnectionString = connString;
+
+            var Page2 = new Window1(); //create your new form.
+            Page2.Show();
+
+
+            //string query = "SELECT COUNT(*) FROM employees";
+            //NpgsqlCommand command = new NpgsqlCommand(query, connection);
+            //int activeConnections = (int)command.ExecuteScalar();
+        }
+
+
+        //private void QueryButton_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+    }
+}
---
+++ WpfApp2/Window1.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Npgsql;
+using WpfApp2;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteQuery_Click(object sender, RoutedEventArgs e)
+        {
+            string query = QueryTextBox.Text;
+            MainWindow mainWindow = new MainWindow();
+            string connString = mainWindow.ConnectionString;
+            using (NpgsqlConnection connection = new NpgsqlConnection(connString))
+            {
+                connection.Open();
+
+                using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                {
+                    using (NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        // Отображение результатов выполненного запроса в ResponseTextBox
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            DataRow row = dataTable.Rows[0];
+                            ResponseTextBox.Text = row[0].ToString(); // Предполагаем, что результат - строка
+                        }
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
+ 
